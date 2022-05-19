0490acfba2cc35863c4363b2fcc6c7a95166a568
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2022-05-19 10:34:18 +0700

    Add project files.

---
+++ GameSudoku/Cons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSudoku
+{
+    class Cons
+    {
+        public static int board_width = 50;
+        public static int board_height = 50;
+    }
+}
---
+++ GameSudoku/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace GameSudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(47, 26);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(580, 580);
+            this.panel.TabIndex = 0;
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolve.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSolve.Location = new System.Drawing.Point(714, 528);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(94, 40);
+            this.btnSolve.TabIndex = 1;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(933, 629);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnSolve;
+    }
+}
+
